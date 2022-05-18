5e2f0e5adb66c9d271f2998ca25a857db590cabd
lalala5772 <65425885+lalala5772@users.noreply.github.com>
2022-05-18 11:48:32 +0900

    fix

--- libft/ft_lstadd_front.c
+++ libft/ft_lstadd_front.c
@@ -1,7 +1,9 @@
 #include "libft.h"
 
-void	ft_lstadd_front(t_list **lst, t_list *new)
+void    ft_lstadd_front(t_list **lst, t_list *new)
 {
-	new->next = *lst;
-	*lst = new;
+        if ((lst == NULL) || (new == NULL))
+                return ;
+        new->next = *lst;
+        *lst = new;
 }
